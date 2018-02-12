b513249e9243b78b5807911a29b0874dcf43b71d
Александр Гриненко <labamifi@gmail.com>
2018-02-12 15:43:59 +0300

    Fixed imports in __init__

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from .code_base_analizer import CodeBaseAnalizer, \
-    CodeBaseParser, ReportDataGenerator  # pragma: no cover
-
-__all__ = ['CodeBaseAnalizer', 'CodeBaseParser',
-           'ReportDataGenerator', 'CodeBaseReportService']  # pragma: no cover
